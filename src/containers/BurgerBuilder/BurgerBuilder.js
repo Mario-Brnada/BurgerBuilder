d8de3964a5a728c2bc676051ff017d19bc4eec79
Mario Brnada <mariodods@gmail.com>
2018-02-19 14:24:36 +0100

    dodo build controls(do 118)

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,23 @@
 import React, { Component, Fragment } from 'react';
 
+import Burger from "../../components/Burger/Burger";
+
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 2,
+      cheese: 0,
+      meat: 0
+    }
+  }
   render() {
     return (
       <Fragment>
-        <div>Burger</div>
-        <div>Build Controls</div>
+        <Burger ingredients={this.state.ingredients} />
+        <BuildControls />
       </Fragment>
     );
   }
